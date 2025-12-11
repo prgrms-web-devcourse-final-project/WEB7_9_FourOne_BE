3acc77380937fff6f95204bc33a11ce5cd6db4b6
KimSihyun <rainy02190@gmail.com>
2025-12-11 14:24:05 +0900

    feat: 회원 탈퇴 30일 후 유저 정보 삭제 스케줄러 추가

--- src/main/java/org/com/drop/domain/user/repository/UserRepository.java
+++ src/main/java/org/com/drop/domain/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package org.com.drop.domain.user.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.com.drop.domain.user.entity.User;
@@ -9,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsByEmail(String email);
 	boolean existsByNickname(String nickname);
 	Optional<User> findByEmail(String email);
+	List<User> findAllByDeletedAtBefore(LocalDateTime dateTime);
 }
