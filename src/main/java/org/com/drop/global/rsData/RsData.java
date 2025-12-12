4ff720bf9aaaa156ebfc899c566059062e75c0d7
kheeyoung <talent8661@gmail.com>
2025-12-12 10:14:59 +0900

    refactor: RsData 수정 - 기본 응답 (200,SUCCESS,요청을 성공적으로 처리 헀습니다) 수정

--- src/main/java/org/com/drop/global/rsData/RsData.java
+++ src/main/java/org/com/drop/global/rsData/RsData.java
@@ -1,15 +1,15 @@
 package org.com.drop.global.rsData;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class RsData<T> {
 
-	private String status;
-	private String code;
-	private String message;
-	private T data;
-
+	private final String code = "SUCCESS";
+	private final String status = "200";
+	private final String message = "요청을 성공적으로 처리했습니다.";
+	private final T data;
+	public RsData(T data) {
+		this.data = data;
+	}
 }
