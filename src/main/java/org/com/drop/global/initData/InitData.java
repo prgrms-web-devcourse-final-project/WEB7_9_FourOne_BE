ae9aeeb49868024c37dd9c2b4af0ad9f7bf6506c
kheeyoung <talent8661@gmail.com>
2025-12-12 10:09:13 +0900

    refactor: initData에 초기 데이터 추가 (경매)

--- src/main/java/org/com/drop/global/initData/InitData.java
+++ src/main/java/org/com/drop/global/initData/InitData.java
@@ -3,6 +3,8 @@ package org.com.drop.global.initData;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.com.drop.domain.auction.auction.dto.AuctionCreateRequest;
+import org.com.drop.domain.auction.auction.service.AuctionService;
 import org.com.drop.domain.auction.product.dto.ProductCreateRequest;
 import org.com.drop.domain.auction.product.entity.Product;
 import org.com.drop.domain.auction.product.service.ProductService;
@@ -23,6 +25,7 @@ public class InitData {
 
 	private final UserRepository userRepository;
 	private final ProductService productService;
+	private final AuctionService auctionService;
 	@Autowired
 	@Lazy
 	private InitData self;
@@ -60,14 +63,34 @@ public class InitData {
 		userRepository.save(user2);
 
 		List<String> images = List.of("img1.png", "img2.png");
-		ProductCreateRequest productCreateRequest = new ProductCreateRequest(
+		ProductCreateRequest productCreateRequest1 = new ProductCreateRequest(
 			"상품명",
 			"설명",
 			Product.Category.STARGOODS,
 			Product.SubCategory.ACC,
 			images
 		);
-		Product product1 = productService.addProduct(productCreateRequest, user1);
+		Product product1 = productService.addProduct(productCreateRequest1, user1);
+
+		ProductCreateRequest productCreateRequest2 = new ProductCreateRequest(
+			"상품명",
+			"설명",
+			Product.Category.STARGOODS,
+			Product.SubCategory.ACC,
+			images
+		);
+		Product product2 = productService.addProduct(productCreateRequest2, user1);
+
+		AuctionCreateRequest auctionCreateRequest = new AuctionCreateRequest(
+			product1.getId(),
+			1000,
+			100000,
+			10,
+			LocalDateTime.now().plusSeconds(5),
+			LocalDateTime.now().plusSeconds(10)
+		);
+		auctionService.addAuction(auctionCreateRequest, user1);
+
 	}
 
 }
