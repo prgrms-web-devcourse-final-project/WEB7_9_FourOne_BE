74512471a23c99d85cef6b7e15bb7d8e370e1c50
kheeyoung <talent8661@gmail.com>
2025-12-15 12:45:44 +0900

    fix : 조회 N+1 문제 수정, 페이징 처리 추가

--- src/main/java/org/com/drop/domain/auction/product/qna/repository/QuestionRepository.java
+++ src/main/java/org/com/drop/domain/auction/product/qna/repository/QuestionRepository.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.com.drop.domain.auction.product.entity.Product;
 import org.com.drop.domain.auction.product.qna.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+	Page<Question> findByProduct(Product product, Pageable pageable);
+
 	List<Question> findByProductOrderById(Product product);
 }
