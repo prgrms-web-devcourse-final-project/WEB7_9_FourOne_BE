a2649114acbc69cc3af0542fbb63e36e5b4ee78a
yenazigeumena <gogguu@bu.ac.kr>
2025-12-17 15:19:31 +0900

    fix: RequestParam -> 커스텀 어노테이션

--- src/main/java/org/com/drop/domain/auction/bid/controller/BidController.java
+++ src/main/java/org/com/drop/domain/auction/bid/controller/BidController.java
@@ -3,12 +3,13 @@ package org.com.drop.domain.auction.bid.controller;
 import org.com.drop.domain.auction.bid.dto.request.BidRequestDto;
 import org.com.drop.domain.auction.bid.dto.response.BidResponseDto;
 import org.com.drop.domain.auction.bid.service.BidService;
+import org.com.drop.domain.user.entity.User;
 import org.com.drop.global.rsdata.RsData;
+import org.com.drop.global.security.auth.LoginUser;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -20,27 +21,15 @@ import lombok.RequiredArgsConstructor;
 public class BidController {
 
 	private final BidService bidService;
-	// private final JwtUtil jwtUtil;
-
 
 	@PostMapping("/{auctionId}/bids")
 	public RsData<BidResponseDto> placeBid(
 		@PathVariable Long auctionId,
-		@RequestParam Long userId,
+		@LoginUser User user,
 		@RequestBody @Valid BidRequestDto requestDto
 	) {
-		BidResponseDto dto = bidService.placeBid(auctionId, userId, requestDto);
+		BidResponseDto dto = bidService.placeBid(auctionId, user.getId(), requestDto);
 		return new RsData<>(dto);
 	}
-
-	// @PostMapping("/{auctionId}/bids")
-	// public RsData<BidResponseDto> placeBid(
-	// 	@RequestHeader("Authorization") String bearerToken,
-	// 	@PathVariable Long auctionId, Long userId,
-	// 	@Valid @RequestBody BidRequestDto request
-	// ) {
-	// 	BidResponseDto dto = bidService.placeBid(auctionId, userId, request);
-	// 	return new RsData<>(dto);
-	// }
 
 }
--- src/main/java/org/com/drop/domain/auction/bid/controller/BuyNowController.java
+++ src/main/java/org/com/drop/domain/auction/bid/controller/BuyNowController.java
@@ -2,11 +2,12 @@ package org.com.drop.domain.auction.bid.controller;
 
 import org.com.drop.domain.auction.bid.dto.response.BuyNowResponseDto;
 import org.com.drop.domain.auction.bid.service.BuyNowService;
+import org.com.drop.domain.user.entity.User;
 import org.com.drop.global.rsdata.RsData;
+import org.com.drop.global.security.auth.LoginUser;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -21,10 +22,10 @@ public class BuyNowController {
 	@PostMapping("/{auctionId}/buy-now")
 	public RsData<BuyNowResponseDto> buyNow(
 		@PathVariable Long auctionId,
-		@RequestParam Long userId
+		@LoginUser User user
 	) {
-		BuyNowResponseDto data = buyNowService.buyNow(auctionId, userId);
-		return new RsData<>(data);
+		BuyNowResponseDto dto = buyNowService.buyNow(auctionId, user.getId());
+		return new RsData<>(dto);
 	}
 
 }
