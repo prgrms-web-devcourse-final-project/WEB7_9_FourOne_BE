223f254f1ca3a00e0e3184faf336d805329a58b1
yenazigeumena <gogguu@bu.ac.kr>
2025-12-17 16:12:25 +0900

    fix: 매핑 주소 수정

--- src/main/java/org/com/drop/domain/auction/bid/controller/BidController.java
+++ src/main/java/org/com/drop/domain/auction/bid/controller/BidController.java
@@ -16,7 +16,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/bids")
+@RequestMapping("/auctions")
 @RequiredArgsConstructor
 public class BidController {
 
