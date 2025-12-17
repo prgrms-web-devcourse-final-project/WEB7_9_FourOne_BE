6407da5b1c8297676b38d77825e6cb81ded861f0
august180918@gmail.com <Lambsteaklover>
2025-12-17 10:16:43 +0900

    chore: configure build and application settings

--- src/main/java/org/com/drop/domain/auth/SecurityConfig.java
+++ src/main/java/org/com/drop/domain/auth/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 	private final JwtFilter jwtFilter;
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) {
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
