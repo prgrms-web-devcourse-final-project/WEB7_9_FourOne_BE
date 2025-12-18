1b30970475cd4d58a9dc2e29d0d88fdc5092901c
yenazigeumena <gogguu@bu.ac.kr>
2025-12-18 12:49:56 +0900

    feat: 입찰 내역

--- src/main/java/org/com/drop/domain/auction/bid/repository/BidRepository.java
+++ src/main/java/org/com/drop/domain/auction/bid/repository/BidRepository.java
@@ -3,8 +3,15 @@ package org.com.drop.domain.auction.bid.repository;
 import java.util.Optional;
 
 import org.com.drop.domain.auction.bid.entity.Bid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BidRepository extends JpaRepository<Bid, Long> {
 	Optional<Bid> findTopByAuction_IdOrderByBidAmountDesc(Long auctionId);
+
+	@EntityGraph(attributePaths = {"bidder"})
+	Page<Bid> findAllByAuctionId(Long auctionId, Pageable pageable);
+
 }
