3b3ca18f775d2fbde0496380d7ad7da731951be1
SiHejt <91488469+SiHejt@users.noreply.github.com>
2025-12-19 13:38:23 +0900

    Update springdoc-openapi dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
 	// Swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
 
 	// Email
 	implementation ("org.springframework.boot:spring-boot-starter-mail")
