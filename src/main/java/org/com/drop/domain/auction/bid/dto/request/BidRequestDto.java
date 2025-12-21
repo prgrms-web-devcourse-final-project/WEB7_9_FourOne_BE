28e123d1fb1e467883a47ee778cce3739c7fa217
yenazigeumena <gogguu@bu.ac.kr>
2025-12-21 17:50:10 +0900

    fix: 주석삭제

--- src/main/java/org/com/drop/domain/auction/bid/dto/request/BidRequestDto.java
+++ src/main/java/org/com/drop/domain/auction/bid/dto/request/BidRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 public record BidRequestDto(
 
 	@NotNull(message = "입찰 금액은 필수입니다.")
-	@Min(value = 1, message = "입찰 금액은 최소 1원 이상이어야 합니다.") // 아주 기본적인 체크
+	@Min(value = 1, message = "입찰 금액은 최소 1원 이상이어야 합니다.")
 	Long bidAmount //입찰금액
 ) {
 }
