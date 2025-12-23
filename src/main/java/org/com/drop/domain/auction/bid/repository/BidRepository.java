955ab105cbf8c69530ded3781ececd5d05ec90d1
KimSihyun <rainy02190@gmail.com>
2025-12-23 10:54:34 +0900

    style: 린트 제거

--- src/main/java/org/com/drop/domain/auction/bid/repository/BidRepository.java
+++ src/main/java/org/com/drop/domain/auction/bid/repository/BidRepository.java
@@ -18,23 +18,25 @@ public interface BidRepository extends JpaRepository<Bid, Long> {
 	@Query("SELECT b.auction.id, COUNT(b) FROM Bid b WHERE b.auction IN :auctions GROUP BY b.auction.id")
 	List<Object[]> countByAuctionIn(@Param("auctions") List<Auction> auctions);
 
-	@Query(value = """
-		SELECT b FROM Bid b
-		JOIN FETCH b.auction a
-		JOIN FETCH a.product p
-		WHERE b.bidder = :user
-		AND b.id IN (
-			SELECT MAX(b2.id)
-			FROM Bid b2
-			WHERE b2.bidder = :user
-			GROUP BY b2.auction.id
-		)
-		ORDER BY b.createdAt DESC
-	""", countQuery = """
-		SELECT COUNT(DISTINCT b.auction.id)
-		FROM Bid b
-		WHERE b.bidder = :user
-	""")
+	@Query(
+		value = """
+				SELECT b FROM Bid b
+				JOIN FETCH b.auction a
+				JOIN FETCH a.product p
+				WHERE b.bidder = :user
+				AND b.id IN (
+					SELECT MAX(b2.id)
+					FROM Bid b2
+					WHERE b2.bidder = :user
+					GROUP BY b2.auction.id
+				)
+				ORDER BY b.createdAt DESC
+				""", countQuery = """
+				SELECT COUNT(DISTINCT b.auction.id)
+				FROM Bid b
+				WHERE b.bidder = :user
+				"""
+	)
 	Page<Bid> findMyLatestBidsPerAuction(@Param("user") User user, Pageable pageable);
 
 	boolean existsByBidderAndAuctionStatus(User bidder, Auction.AuctionStatus status);
