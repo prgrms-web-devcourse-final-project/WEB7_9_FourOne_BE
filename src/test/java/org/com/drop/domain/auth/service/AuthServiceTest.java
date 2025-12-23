8dd3ff51146dd5c4eabbbc727710ca850ebef7cb
KimSihyun <rainy02190@gmail.com>
2025-12-23 11:29:04 +0900

    test: 테스트 mock 객체 주입

--- src/test/java/org/com/drop/domain/auth/service/AuthServiceTest.java
+++ src/test/java/org/com/drop/domain/auth/service/AuthServiceTest.java
@@ -6,6 +6,8 @@ import static org.mockito.Mockito.*;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.com.drop.domain.auction.auction.repository.AuctionRepository;
+import org.com.drop.domain.auction.bid.repository.BidRepository;
 import org.com.drop.domain.auth.dto.GetCurrentUserInfoResponse;
 import org.com.drop.domain.auth.dto.LocalLoginRequest;
 import org.com.drop.domain.auth.dto.LocalLoginResponse;
@@ -65,6 +67,12 @@ class AuthServiceTest {
 
 	@Mock
 	private VerificationCodeStore verificationCodeStore;
+
+	@Mock
+	private AuctionRepository auctionRepository;
+
+	@Mock
+	private BidRepository bidRepository;
 
 	private User mockUser;
 
