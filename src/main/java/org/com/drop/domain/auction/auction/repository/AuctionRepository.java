16b5addf278ec6c45f15f6fd87e0c3b5e22643e9
yenazigeumena <gogguu@bu.ac.kr>
2025-12-29 15:42:30 +0900

    feat: Jpa Lock

--- src/main/java/org/com/drop/domain/auction/auction/repository/AuctionRepository.java
+++ src/main/java/org/com/drop/domain/auction/auction/repository/AuctionRepository.java
@@ -8,6 +8,11 @@ import org.com.drop.domain.auction.auction.entity.Auction;
 import org.com.drop.domain.auction.product.entity.Product;
 import org.com.drop.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.persistence.LockModeType;
 
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
 	Optional<Auction> findByProductId(Long id);
@@ -19,4 +24,8 @@ public interface AuctionRepository extends JpaRepository<Auction, Long> {
 	List<Auction> findAllByStatusAndStartAtBefore(Auction.AuctionStatus status, LocalDateTime now);
 
 	List<Auction> findAllByStatusAndEndAtBefore(Auction.AuctionStatus status, LocalDateTime now);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT a FROM Auction a WHERE a.id = :id")
+	Optional<Auction> findByIdWithPessimisticLock(@Param("id") Long id);
 }
