a67e9a54090b8c763ea025ce93abf21e18f28b12
kheeyoung <talent8661@gmail.com>
2025-12-29 14:10:24 +0900

    fix : DropApplicationTests의 오류를 확인 -> aws 계정 있으면 해결됨. 일단 주석 처리 하고 필요시 담당자에게 연락

--- src/test/java/org/com/drop/DropApplicationTests.java
+++ src/test/java/org/com/drop/DropApplicationTests.java
@@ -4,8 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DropApplicationTests {
-	// @Test
-	// void contextLoads() {
-	//
-	// }
+	 // @Test
+	 // void contextLoads() {
+	 // }
 }
