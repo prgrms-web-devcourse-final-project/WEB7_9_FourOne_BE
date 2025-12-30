48a524cc068520628949dad64e80b7cb84f569f8
kheeyoung <talent8661@gmail.com>
2025-12-30 13:22:38 +0900

    feat : Spring Actuator 보안 설정 통과하게 설정

--- src/main/java/org/com/drop/domain/auth/config/SecurityConfig.java
+++ src/main/java/org/com/drop/domain/auth/config/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig {
 					"/api/v1/auth/email/verify-code",
 					"/api/v1/auth/email/send-code",
 					"/api/v1/auth/refresh",
-					"/api/v1/admin/help"
+					"/api/v1/admin/help",
+					"/actuator/**"
 				).permitAll()
 				.requestMatchers("/api/v1/auth/logout").authenticated()
 				.requestMatchers(HttpMethod.GET, "/api/v1/auctions/**").permitAll()
