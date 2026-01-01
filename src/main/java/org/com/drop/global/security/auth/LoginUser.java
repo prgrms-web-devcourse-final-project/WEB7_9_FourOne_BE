67b6da02599643d0f50a2e68a44c02f836a704a3
BE9-KNH <liorex319@gmail.com>
2026-01-02 06:40:36 +0900

    fix: 비로그인 사용자 경매 목록/상세 조회 버그 수정 및 북마크 카운트 증감메서드 추가
    # Conflicts:
    #	src/main/java/org/com/drop/domain/auth/config/SecurityConfig.java

--- src/main/java/org/com/drop/global/security/auth/LoginUser.java
+++ src/main/java/org/com/drop/global/security/auth/LoginUser.java
@@ -7,4 +7,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface LoginUser { }
+public @interface LoginUser {
+	boolean required() default true;
+}
