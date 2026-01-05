881502d0ffcf987e35cbe01193ea7d204611420b
kheeyoung <talent8661@gmail.com>
2026-01-05 11:08:48 +0900

    fix : 테스트 disabled 삭제

--- src/test/java/org/com/drop/DropApplicationTests.java
+++ src/test/java/org/com/drop/DropApplicationTests.java
@@ -1,11 +1,9 @@
 package org.com.drop;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 class DropApplicationTests {
 	@Test
 	void contextLoads() {
