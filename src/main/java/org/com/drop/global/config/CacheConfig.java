e89ee78125a5cb239276e35e5b2b57dd9be69283
kheeyoung <talent8661@gmail.com>
2026-01-05 19:47:45 +0900

    fix : 누락 되었던 cacheManager 추가

---
+++ src/main/java/org/com/drop/global/config/CacheConfig.java
@@ -0,0 +1,23 @@
+package org.com.drop.global.config;
+
+import java.util.List;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+	@Bean
+	public CacheManager cacheManager() {
+		SimpleCacheManager cacheManager = new SimpleCacheManager();
+		cacheManager.setCaches(List.of(
+			new ConcurrentMapCache("product:detail"),
+			new ConcurrentMapCache("product:list")
+		));
+		return cacheManager;
+	}
+}
